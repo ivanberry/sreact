ddd3ffa868135873ddec3cdaf29b2aa1c064f5d0
ivanberry <tabvim.dev@gmail.com>
2018-06-07 12:04:07 +0800

    Add demo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,56 @@
 import React from "./lib/react";
 
-const rootDom = document.getElementById("root");
-function tick() {
-    const time = new Date().toLocaleTimeString();
-    const clockElement = <p id="test">Time is: {time}</p>;
-    React.render(clockElement, rootDom);
+const randomLikes = () => Math.ceil(Math.random() * 100);
+
+const stories = [
+    {
+        name: "Didact introduction",
+        url: "http://bit.ly/2pX7HNn",
+        likes: randomLikes()
+    },
+    {
+        name: "Rendering DOM elements ",
+        url: "http://bit.ly/2qCOejH",
+        likes: randomLikes()
+    },
+    {
+        name: "Element creation and JSX",
+        url: "http://bit.ly/2qGbw8S",
+        likes: randomLikes()
+    },
+    {
+        name: "Instances and reconciliation",
+        url: "http://bit.ly/2q4A746",
+        likes: randomLikes()
+    },
+    {
+        name: "Components and state",
+        url: "http://bit.ly/2rE16nh",
+        likes: randomLikes()
+    }
+];
+
+const appElement = () => (
+    <div>
+        <ul>{stories.map(storyElement)}</ul>
+    </div>
+);
+
+function storyElement(story) {
+    return (
+        <li>
+            <button onClick={e => handleClick(story)}>
+                {story.likes}
+                <b>❤️</b>
+            </button>
+            <a href={story.url}>{story.name}</a>
+        </li>
+    );
 }
 
-tick();
-setInterval(tick, 1000);
+function handleClick(story) {
+    story.likes += 1;
+    React.render(appElement(), document.getElementById("root"));
+}
+
+React.render(appElement(), document.getElementById("root"));
